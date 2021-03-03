c72884a870e826ec2596336b2919c169f6753be2
cdgaete <cdgaete@gmail.com>
2021-03-03 20:13:26 +0100

    v0.2.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Carlos Gaete-Morales, Martin Kittel, Alexander Roth, Wolf-Pet
 author = "Carlos Gaete-Morales, Martin Kittel, Alexander Roth, Wolf-Peter Schill, Alexander Zerrahn"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.2"
+release = "0.2.3"
 
 
 # -- General configuration ---------------------------------------------------
